8bc6b058b80b673e35db55b877b0e633d63ebe15
TrainingBasket <sourcecode@trainingbasket.in>
2020-05-29 18:06:03 +0530

    New view to show all tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from .models import Task
 
 def home(request):
 	a = {"ENGLISH":87,"MATHS":94,"SCIENCE":88}
 	return render(request,'index.html',{'subjects':a})
+
+
+def alltasks(request):
+	t = Task.objects.all()
+	return render(request,"alltasks.html",{'alltasks':t})
